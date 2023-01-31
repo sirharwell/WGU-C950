8b48ff6cc8583596f1fc178c2bf08fd7055ac10e
sirharwell <36307972+sirharwell@users.noreply.github.com>
2023-01-30 23:08:32 -0700

    Create HashTable.py
    Hash table

---
+++ HashTable.py
@@ -0,0 +1,38 @@
+#Ian Harwell 000683773
+# implemented using c950 Webinar 1 as reference (Tepe, 17 Nov. 2020)
+
+# Initialization  ------------------------------------------------------------------------------------------------------
+
+class ChainingHashTable:
+
+    def __init__(self, initial_capacity=10):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    def insert(self, key, item):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        for kv in bucket_list:
+            if kv[0] == key:
+                kv[1] = item
+                return True
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+
+    def search(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1]  # value
+            return None
+
+    def remove(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+        for kv in bucket_list:
+            if kv[0] == key:
+                bucket_list.remove([kv[0], kv[1]])
